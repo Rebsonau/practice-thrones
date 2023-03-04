ab927f9845bdf88c862282aa1171aab34cb2d769
Lee Kelly <leeowenkelly@gmail.com>
2023-03-04 16:44:19 +0000

    GoT file added

---
+++ GoT.js
@@ -0,0 +1,19 @@
+const character = [{
+    firstName: "Ned",
+    house: "Stark",
+    houseWords: "Winter is coming",
+    houseSigil: "Direwolf"
+}, {
+    firstName: "Robert",
+    house: "Baratheon",
+    houseWords: "Ours is the fury",
+    houseSigil: "Stag"
+}, {
+    firstName: "Tywin",
+    house: "Lannister",
+    houseWords: "Hear me roar"
+    houseSigil: "Lion"
+}]
+
+console.log("House:", character[0].house)
+console.log("House Words:", chatacter[0].houseWords)
